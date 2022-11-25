2189d7fefe34db1a0094c987141d93c4a08939e0
Hung Nguyen <61287649+nguyenhungitc@users.noreply.github.com>
2022-11-25 07:53:55 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Create user for replication
+mysql -u root --password=$MARIADB_ROOT_PASSWORD --execute="CREATE USER IF NOT EXISTS '$MARIADB_REPL_USER'@'$MASTER_HOST_ADDRESS' \
+IDENTIFIED BY '$MARIADB_REPL_PASSWORD'; GRANT REPLICATION SLAVE ON *.* TO '$MARIADB_REPL_USER'@'$MASTER_HOST_ADDRESS'; FLUSH PRIVILEGES;"
+
 # Get the log position and name.
 master_result=$(mysql -u root --password=$MARIADB_ROOT_PASSWORD --execute="reset master; show master status;")
 master_log=$(echo $master_result | awk '{print $5}')
